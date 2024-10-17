a98d28abb494ee23233cd8b151d523a0eacdeb3e
Valeriia Zubrytska <valeriia.zubrytska@gmail.com>
2024-10-17 15:47:32 +0300

    add persist

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,6 @@ import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
-  // const handleChange = (evt) => {
-  //   setFilterValue(evt.target.value);
-  // };
-
-  // const searchValue = contacts.filter((contact) =>
-  //   contact.name.toLowerCase().includes(filterValue.toLowerCase())
-  // );
-
   return (
     <>
       <h1>PhoneBook</h1>
